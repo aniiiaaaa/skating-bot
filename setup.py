023b200249b6471c9370e1f9f58988b94dfe9ddc
aniiiaaaa <57152437+aniiiaaaa@users.noreply.github.com>
2023-02-22 10:56:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,11 +15,13 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
-#driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-link = "https://app.cituro.com/booking/scc#step=1"
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+driver2 = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+driver3 = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 wait = WebDriverWait(driver, 20)
+wait2 = WebDriverWait(driver2, 20)
+wait3 = WebDriverWait(driver3, 20)
 
 def telegram_bot_sendtext(bot_message):
    bot_token = os.getenv("BOT_TOKEN")
@@ -29,39 +31,80 @@ def telegram_bot_sendtext(bot_message):
    response = requests.get(send_text)
    return response.json()
 
-# compare the request's html
-while True:
-    try:
-        driver.get(link)
-        wait.until(EC.visibility_of_element_located(
-            (By.XPATH, '/html/body/div/section/div[2]/div[2]/div/div/div[4]/div[2]/div/div[6]'))).click()
-        page_html = wait.until(EC.visibility_of_element_located(
-            (By.XPATH, '/html/body/div/section/div[2]/div[2]/div[2]/div/div[2]/div'))).get_attribute('innerText')
-        time.sleep(900) #900 seconds = 15min
-        driver.refresh()  # refresh page
-        wait.until(EC.visibility_of_element_located(
-            (By.XPATH, '/html/body/div/section/div[2]/div[2]/div/div/div[4]/div[2]/div/div[6]'))).click()
-        page_html2 = wait.until(EC.visibility_of_element_located(
-            (By.XPATH, '/html/body/div/section/div[2]/div[2]/div[2]/div/div[2]/div'))).get_attribute('innerText')
-        # driver.close()
-        chrome_options = Options()
-        if page_html == page_html2:
-            #test = telegram_bot_sendtext('nothing changed ' + page_html)
-            driver.refresh()
-            continue
+#SCC Free training
+def check_scc():
+    while True:
+        try:
+            driver.get("https://app.cituro.com/booking/scc#step=1")
+            wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div/section/div[2]/div[2]/div/div/div[4]/div[2]/div/div[6]'))).click()
+            page_html = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div/section/div[2]/div[2]/div[2]/div/div[2]/div'))).get_attribute('innerText')
+            time.sleep(30) #900 seconds = 15min
+            driver.refresh()  # refresh page
+            wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div/section/div[2]/div[2]/div/div/div[4]/div[2]/div/div[6]'))).click()
+            page_html2 = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div/section/div[2]/div[2]/div[2]/div/div[2]/div'))).get_attribute('innerText')
+            chrome_options = Options()
+            if page_html == page_html2:
+                telegram_bot_sendtext('nothing changed SCC')
+                driver.refresh()
+                continue
+            else:
+                telegram_bot_sendtext('SCC Free Training: ' + page_html2)
+                driver.refresh()
+                time.sleep(60)
+                continue
+        except Exception as e:
+            print("error",e)
 
-        # if something changed in the hashes
-        else:
-            print(page_html2)
-            wait = WebDriverWait(driver, 20)
+#BEV PHS
+def check_bev_phs():
+    while True:
+        try:
+            driver2.get("https://app.cituro.com/booking/bev#step=1")
+            wait2.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div/section/div[2]/div[2]/div/div/div[5]/div/div'))).click()
+            page_html3 = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div/section/div[2]/div[2]/div[2]/div/div[2]/div'))).get_attribute('innerText')
+            time.sleep(30) #900 seconds = 15min
+            driver2.refresh()  # refresh page
+            wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div/section/div[2]/div[2]/div/div/div[5]/div/div'))).click()
+            page_html4 = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div/section/div[2]/div[2]/div[2]/div/div[2]/div'))).get_attribute('innerText')
+            chrome_options = Options()
+            if page_html3 == page_html4:
+                telegram_bot_sendtext('nothing changed BEV PHS')
+                driver2.refresh()
+                continue
+            else:
+                telegram_bot_sendtext('BEV PHS: ' + page_html4)
+                driver2.refresh()
+                time.sleep(60)
+                continue
+        except Exception as e:
+            print("error",e)
 
-            print(page_html2)
-            telegram_bot_sendtext('SCC Free Training: ' + page_html2)
 
-            # wait for 30 seconds
-            time.sleep(60)
-            continue
+#BEV EHE(Halle)
+def check_bev_ehe():
+    while True:
+        try:
+            driver3.get("https://app.cituro.com/booking/bev#step=1")
+            wait3.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div/section/div[2]/div[2]/div/div/div[2]/div/div'))).click()
+            page_html5 = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div/section/div[2]/div[2]/div[2]/div/div[2]/div'))).get_attribute('innerText')
+            time.sleep(30) #900 seconds = 15min
+            driver3.refresh()  # refresh page
+            wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div/section/div[2]/div[2]/div/div/div[2]/div/div'))).click()
+            page_html6 = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div/section/div[2]/div[2]/div[2]/div/div[2]/div'))).get_attribute('innerText')
+            chrome_options = Options()
+            if page_html5 == page_html6:
+                telegram_bot_sendtext('nothing changed BEV EHE')
+                driver3.refresh()
+                continue
+            else:
+                telegram_bot_sendtext('BEV EHE: ' + page_html6)
+                driver3.refresh()
+                time.sleep(60)
+                continue
+        except Exception as e:
+            print("error",e)
 
-    # To handle exceptions
-    except Exception as e:
-        print("error",e)
+#run
+thread1 = threading.Thread(target=check_scc).start()
+thread2 = threading.Thread(target=check_bev_phs).start()
+thread3 = threading.Thread(target=check_bev_ehe).start()
